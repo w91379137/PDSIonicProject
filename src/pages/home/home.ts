d623b7a038ca36bb62ad4c649cbeb05183476f04
林永璋 <yclin@jumbogames.com.tw>
2017-12-15 17:15:37 +0800

    新增原生相機截圖

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AboutPage } from '../about/about';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,9 +11,30 @@ import { AboutPage } from '../about/about';
 export class HomePage {
 
   aboutPage = AboutPage;
+  camera;
 
   constructor(public navCtrl: NavController) {
-
+    this.camera = new Camera()
   }
 
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      var image = document.getElementById('myImage') as HTMLImageElement;
+      image.src = base64Image;
+
+    }, (err) => {
+      // Handle error
+    });
+  }
 }
+

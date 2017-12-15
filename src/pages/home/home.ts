d4a51378ee87c76e4f7a234a377675b518df4496
林永璋 <yclin@jumbogames.com.tw>
2017-12-15 15:41:40 +0800

    新增 about navi 過去

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  aboutPage = AboutPage;
 
   constructor(public navCtrl: NavController) {
 

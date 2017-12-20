1fc48cefb393929801530734cd9845a9de8731c9
林永璋 <yclin@jumbogames.com.tw>
2017-12-20 11:29:04 +0800

    自己測試

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,32 @@ import { Device } from '@ionic-native/device';
 //import { Keyboard } from 'ionic-plugin-keyboard';
 //import { Storage } from '@ionic/storage';
 //import { IonicNativePlugin } from '@ionic-native/core';
-import { Geolocation } from '@ionic-native/geolocation';
+//import { Geolocation } from '@ionic-native/geolocation';
+
+// import { Injectable } from '@angular/core';
+// import { Plugin, Cordova, CordovaProperty, CordovaInstance, InstanceProperty, IonicNativePlugin } from '@ionic-native/core';
+// import { Observable } from 'rxjs/Observable';
+
+// @Plugin({
+//   pluginName: 'MyCordovaPlugin',
+//   plugin: 'my-cordova-plugin', // npm package name, example: cordova-plugin-camera
+//   pluginRef: 'mycordovaplugin', // the variable reference to call the plugin, example: navigator.geolocation
+//   repo: '', // the github repository URL for the plugin
+//   install: '', // OPTIONAL install command, in case the plugin requires variables
+//   installVariables: [], // OPTIONAL the plugin requires variables
+//   platforms: ['iOS'] // Array of platforms supported, example: ['Android', 'iOS']
+// })
+// @Injectable()
+// export declare class MyCordovaPlugin extends IonicNativePlugin {
+
+//   // @Cordova()
+//   // functionName(arg1: string, arg2: number): Promise<any> {
+//   //     return; // We add return; here to avoid any IDE / Compiler errors
+//   // }
+//   echo(arg1: string, arg2: string): void;
+// }
+
+declare let cordova: any;   //ionic2调用自定义插件这里这么写
 
 @Component({
   selector: 'page-home',
@@ -24,13 +49,21 @@ export class HomePage {
   }
 
   takePicture(){
-
+    
     if (window["cordova"] == undefined) {
+      alert("no cordova");
       return
     }
+    
+    // var p = new MyCordovaPlugin();
+    // p.echo("arg3", "arg4");
+    cordova.plugins.MyCordovaPlugin.echo("arg3", "arg4", result => {
+      alert(result);
+    }, error => alert(error))
 
     var device = new Device();
     if (device.platform !== 'ios') {
+      alert("no ios");
       return
     }
 
